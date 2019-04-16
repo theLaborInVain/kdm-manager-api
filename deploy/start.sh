38e9f474cea74be0cf0e9ff77c0f5d2e4e8732c6
Timothy O'Connell <toconnell@tyrannybelle.com>
2019-04-16 16:25:22 -0500

    API is a constant now

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,4 +3,4 @@
 pushd /home/toconnell/kdm-manager-api
 source venv/bin/activate
 export FLASK_ENV=production
-venv/bin/gunicorn -b localhost:8013 -w 4 app:api
+venv/bin/gunicorn -b localhost:8013 -w 4 app:API
